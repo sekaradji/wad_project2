976812761ccf17da76349b1db5c24946ee7823c5
sekaradji <105697602+sekaradji@users.noreply.github.com>
2023-10-19 17:44:38 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan1</title>
+</head>
+<body>
+    <main>
+      <h1>Hello World</h1>
+      <?php
+      echo "Selamat Datang Di Kelas Pengembangan Aplikasi Website"
+
+      $angka_pertama = 5; //integer
+      $var_string = "kasih huruf"; //string
+      $var_double = 4.5; //double / float
+      $var_bool = true; //boolean
+      $var_char = 'a'; //char
+
+      //kondisi variabel
+      $var_null = null; //null
+
+      //operator
+      $angka_pertama + $var_double; // +
+      $angka_pertama - $var_double; // -
+
+      //percabangan
+      if (true) {
+        echo $var_string;
+      }
+
+      if (false) {
+        echo $var_string
+      } else {
+        echo $var_char
+      }
+
+      //perulangan
+      ?>
+</body>
+</html>
